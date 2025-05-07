97c96fc1dc3b0ca592a6c621fba30ef15e7b0bf8
scpa06001 <scpa06001@utb.vaxjo.se>
2025-05-07 09:30:12 +0200

    1234567890

--- Script2.js
+++ Script2.js
@@ -4,20 +4,25 @@ let player_x = []
 let player_o = []
 
 function clickruta(id){
-    console.log(id);
+    console.log("id:"+id);
     if ((player_x.length + player_o.length) % 2 == 0){
-        rutor[id].innerHTML = "X"
-        player_x.push(id)
+        if (!player_x.includes(id) && !player_o.includes(id)){
+            rutor[id].innerHTML = "X"
+            player_x.push(id)
+        }     
         console.log(player_x)
     }else{
-        rutor[id].innerHTML = "O"
-        player_o.push(id)
+        if (!player_x.includes(id) && !player_o.includes(id)){
+            rutor[id].innerHTML = "O"
+            player_o.push(id)
+        }
         console.log(player_o)
     }
-   
 }
 
-
-
-
+function CheckTheWin(){
+        [0,1,2], [3,4,5], [6,7,8],
+        [0,3,6], [1,4,7], [2,5,8], 
+        [0,4,8], [2,4,6]
+}
 
