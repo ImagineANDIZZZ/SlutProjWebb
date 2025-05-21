02d8977339565d38b7ae7103f7c7b56b99df31da
scpa06001 <scpa06001@utb.vaxjo.se>
2025-05-21 09:37:39 +0200

    1234567890

--- Script2.js
+++ Script2.js
@@ -1,3 +1,7 @@
+function isSubset(subset, array) {
+    return subset.every(element => array.includes(element));
+  }
+
 let rutor = document.getElementsByClassName("ruta");
 
 let player_x = []
@@ -9,25 +13,33 @@ function clickruta(id){
         if (!player_x.includes(id) && !player_o.includes(id)){
             rutor[id].innerHTML = "X"
             player_x.push(id)
+            console.log(CheckTheWin("x"));
         }     
         console.log(player_x)
         }else{
         if (!player_x.includes(id) && !player_o.includes(id)){
             rutor[id].innerHTML = "O"
             player_o.push(id)
+            CheckTheWin("o");
         }
         console.log(player_o)
         
     }
-    CheckTheWin();
+  
 }
 
-function CheckTheWin() {
-    const vinnandeKombinationer = [0,1,2]
- /*        [0, 1, 2], [3, 4, 5], [6, 7, 8],
-        [0, 3, 6], [1, 4, 7], [2, 5, 8],
-        [0, 4, 8], [2, 4, 6]
-    ]; */
+function CheckTheWin(id) {
+    const vinnandeKombinationer = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+           [0, 3, 6], [1, 4, 7], [2, 5, 8],
+           [0, 4, 8], [2, 4, 6]
+       ];
+
+    if (player_x.length >= 3){
+          
+        }
+
+    }
+
 
     let result = vinnandeKombinationer.every(val=> player_o.includes(val))
     console.log(result)
